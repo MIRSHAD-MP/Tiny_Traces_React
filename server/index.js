47bbe905618a9531b021afc40cf882ac449d1102
Mirshad Mp <109660034+MIRSHAD-MP@users.noreply.github.com>
2023-02-06 14:17:57 +0530

    Frontend Installation and Setup

--- server/index.js
+++ server/index.js
@@ -15,6 +15,9 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -59,4 +62,8 @@ mongoose
     useUnifiedTopology: true,
 }).then(() => {
     app.listen(PORT, () => console.log(`SERVER PORT: ${PORT}`));
+
+//ADD DATA ONE TIME    
+    // User.insertMany(users);
+    // Post.insertMany(posts);
 }).catch((error) => console.log(`${error} DID NOT CONNECT`))
